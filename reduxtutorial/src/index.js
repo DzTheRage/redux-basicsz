0eac45eb194d81ffdd7ee1ab9cbb70bbcf6f7fa3
Scott Vance <32145716+DzTheRage@users.noreply.github.com>
2018-01-03 13:51:51 -0600

    playing with reducs

--- reduxtutorial/src/index.js
+++ reduxtutorial/src/index.js
@@ -1,30 +1,49 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import { User } from './components/User';
-import { Main } from './components/Main';
+// import React from "react";
+// import ReactDOM from 'react-dom';
+// import { User } from './components/User';
+// import { Main } from './components/Main';
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "Max"
-        };
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             username: "Max"
+//         };
+//     }
+
+//     changeUsername(newName) {
+//         this.setState({
+//             username: newName
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div className="container">
+//                 <Main changeUsername={this.changeUsername.bind(this)}/>
+//                 <User username={this.state.username}/>
+//             </div>
+//         );
+//     }
+// }
+
+// ReactDOM.render(<App />, window.document.getElementById('root'));
+
+import { createStore } from "redux";
+
+const reducer = (state,action) => {
+    switch (action.type) {
+        case "ADD": 
+            break;
+        case "SUBTRACT":
+            break;
     }
+    return state;
+};
 
-    changeUsername(newName) {
-        this.setState({
-            username: newName
-        });
-    }
+const store = createStore(reducer, 1);
 
-    render() {
-        return (
-            <div className="container">
-                <Main changeUsername={this.changeUsername.bind(this)}/>
-                <User username={this.state.username}/>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App />, window.document.getElementById('root'));
+store.dispatch({
+    type: "ADD",
+    payload: 2,
+});
