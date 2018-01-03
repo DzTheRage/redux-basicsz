4a7c18c2b1d388bb7c9bb83917ebdb7073684ab2
Scott Vance <32145716+DzTheRage@users.noreply.github.com>
2018-01-03 10:32:36 -0600

    Fixed Intial Setup

---
+++ reduxtutorial/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import { User } from './components/User';
+import { Main } from './components/Main';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "Max"
+        };
+    }
+
+    changeUsername(newName) {
+        this.setState({
+            username: newName
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Main changeUsername={this.changeUsername.bind(this)}/>
+                <User username={this.state.username}/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, window.document.getElementById('root'));
